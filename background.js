83ddb2e99a26c7c974dff1f6a40352ba90654dd6
keventapi <lohankeven983@outlook.com>
2025-05-09 15:05:47 -0300

    update

--- background.js
+++ background.js
@@ -1,94 +1,132 @@
 const your_ipv4_ip = '192.168.5.102';
 var get_reccomendation = true
+
 setInterval(() => {
-    fetch(`http://${your_ipv4_ip}:5000/change_video`)
+    fetch(`http://${your_ipv4_ip}:5000/get_data`)
       .then(res => res.json())
       .then(data => {
         if (data.executar_algo) {
           chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
             const tab = tabs[0];
-            if (tab && tab.url.includes("youtube.com")) {
-              chrome.tabs.sendMessage(tab.id, {
-                action: "change_video",
-                url: data.url
-              }, (res) => {
-                if (chrome.runtime.lastError) {
-                  console.error("Erro ao enviar mensagem:", chrome.runtime.lastError.message);
-                } else {
-                  console.log('ok, enviando');
-                  fetch(`http://${your_ipv4_ip}:5000/changed`);
-
-                  // Depois de redirecionar, enviar mensagem para pegar as recomendações
-                  // Espere o content.js coletar as recomendações antes de enviar
-                  setTimeout(() => {
-                    chrome.tabs.sendMessage(tab.id, {
-                      action: "collect_recommendations"
-                    }, (response) => {
-                      if (chrome.runtime.lastError) {
-                        console.error("Erro ao enviar mensagem:", chrome.runtime.lastError.message);
-                      } else {
-                        data_json = {}
-                        for (let i = 0; i < response.data.length; i++){
-                          data_json[`video${i+1}`] = response.data[i]['url']
-                        }
-                        fetch('http://192.168.5.102:5000/reccomendations', {method: "POST",
-                          headers: {
-                            "Content-Type": "application/json"
-                          },
-                          body: JSON.stringify(data_json)
-                        })
-                        console.log("Recomendações recebidas do content script:", data_json);
-                      }
-                    });
-                  }, 4000); // Espera 4 segundos para garantir que as recomendações foram carregadas
-                }
-              });
+            if (tab && tab.url.includes("youtube.com/watch?v")) {
+              if(data.function == "change_video"){
+                change_video(data, tab);
+              }
+              if(data.function == "next"){
+                next(data, tab);
+              }
+              if(data.function == "pause"){
+                pause(data, tab);
+              }
             } else {
               console.warn("Nenhuma aba do YouTube ativa encontrada.");
             }
-          });
+          }
+        );
         }
       })
       .catch(err => console.error('Erro ao verificar tarefas:', err));
 }, 5000);
 
+function change_video(data, tab){
+  chrome.tabs.sendMessage(tab.id, {
+    action: "change_video",
+    url: data.url
+  }, (res) => {
+    if (chrome.runtime.lastError) {
+      console.error("Erro ao mudar video por url/recomendação:", chrome.runtime.lastError.message);
+    } else {
+      fetch(`http://${your_ipv4_ip}:5000/changed`);
+      set_reccomendation()
+    }
+  });
+}
+
+function next(data, tab){
+  chrome.tabs.sendMessage(tab.id, {action: "next"}, (res) => {
+    if(chrome.runtime.lastError){
+      console.log('erro ao executar proximo video', chrome.runtime.lastError.message)
+    }else{
+      fetch(`http://${your_ipv4_ip}:5000/changed`)
+      set_reccomendation()
+    }
+  })
+}
+
+function pause(data, tab){
+  chrome.tabs.sendMessage(tab.id, {action: "pause"}, (res) => {
+    if(chrome.runtime.lastError){
+      console.log("erro ao tentar pausar ou despausar o video", chrome.runtime.lastError.message)
+    }else{
+      fetch(`http://${your_ipv4_ip}:5000/changed`).then((response) => response.json()).then((data) => {
+        console.log(data);
+      })
+    }
+  })
+}
+
 function onload_get_reccomendation(){
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
       const tab = tabs[0]
-      if (tab && tab.url.includes('youtube.com')){
+      if (tab && tab.url.includes('youtube.com/watch?v')){
         chrome.tabs.sendMessage(tab.id, {action: 'collect_recommendations'}, (response) => {
           if (response.status === 'ok' && response.data){
-            if (get_reccomendation){
-              get_reccomendation = false
-              console.log(response.data)
-              data_json = {}
-              for (let i = 0; i < response.data.length; i++){
-                data_json[response.data[i]['titulo']] = {url: response.data[i]['url'], thumb: ''}
-                console.log(data_json)
-              }
-              fetch('http://192.168.5.102:5000/reccomendations', {method: "POST",
-                headers: {
-                "Content-Type": "application/json"},
-                body: JSON.stringify(data_json)
-              })
-          }
+
+            data_json = {}
+            for (let i = 0; i < response.data.length; i++){
+              data_json[response.data[i]['titulo']] = {"url": response.data[i]['link'], "thumb": response.data[i]['thumb']}
+            }
+
+            fetch(`http://${your_ipv4_ip}:5000/get_data`).then((res) => res.json()).then((data) => {
+              if (get_reccomendation || data.recomendations == [] || data_json != data.recomendations){
+                get_reccomendation = false
+                console.log(response.data)
+                
+                fetch(`http://${your_ipv4_ip}:5000/reccomendations`, {method: "POST",
+                  headers: {
+                  "Content-Type": "application/json"},
+                  body: JSON.stringify(data_json)
+                })
+            }
+
+            })
+            onload_get_volume();
           }
         })
       }else{
-        console.warn('nn foi identificada nhm guia do youtube')
-      }
-      if(!get_reccomendation){
-        console.log('isso deve ser mostrado apenas uma vez')
+        console.warn('a guia atual não é do youtube')
       }
     })
 }
 
+function onload_get_volume(){
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+    const tab = tabs[0]
+    if(tab&& tab.url.includes('youtube.com/watch?v')){
+      chrome.tabs.sendMessage(tab.id, {action: 'get_volume'}, (response) => {
+        console.log(response)
+        if(response.status == 'ok' && response.volume !== undefined){
+          fetch(`http://${your_ipv4_ip}:5000/post_volume`, {method: "POST",
+            headers: {"Content-Type": "application/json"}, body: JSON.stringify({'volume': response.volume}) } )
+        }
+      })
+    }
+  })
+}
+
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   if (changeInfo.status == 'complete') {
-    if(tab.url.includes('youtube.com')){
+    if(tab.url.includes('youtube.com/watch?v')){
       get_reccomendation = true
     }
    }
 });
+
+function set_reccomendation(){
+  setTimeout(() => {
+    get_reccomendation = true;
+    onload_get_reccomendation();
+  }, 5000)
+}
 
 var get_reccomendation_interval =  setInterval(onload_get_reccomendation , 4000)
