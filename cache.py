63c439f87fb6f172056625ee239f3d08268184bd
keventapi <lohankeven983@outlook.com>
2025-06-15 02:32:25 -0300

    adicionado um limite por ip para restringir a criação de contas

--- cache.py
+++ cache.py
@@ -13,16 +13,27 @@ def start_required(f):
     return wrapped
 
 class CacheHandler:
-    def __init__(self, limit, time_stamp, expire_limit=5, event_expire_time=0.5, host="localhost", port=6379):
+    def __init__(self, login_limit, login_time_stamp_in_seconds, login_expire_limit_in_minutes=5,
+                 event_expire_time_in_seconds=0.5, 
+                 create_account_limit=5, create_account_time_stamp_in_days=1, create_account_expire_limit_in_days=30,
+                 host="localhost", port=6379):
         try:
-            if self.check_redis_parameters(host, port) and self.check_initial_parameters(limit, time_stamp, expire_limit, event_expire_time):
+            if self.check_if_its_a_number_parameters(login_limit, login_time_stamp_in_seconds, login_expire_limit_in_minutes,
+                                                     event_expire_time_in_seconds,
+                                                     create_account_limit, create_account_time_stamp_in_days, create_account_expire_limit_in_days) and self.check_redis_parameters(host, port):
                 self.r = None
                 self.host = host
                 self.port = port
-                self.limit = limit
-                self.time_stamp = time_stamp
-                self.expire_limit = expire_limit*60
-                self.event_expire_time = event_expire_time
+                
+                self.login_limit = login_limit
+                self.login_time_stamp_in_seconds = login_time_stamp_in_seconds
+                self.login_expire_limit_in_minutes = self.minutes_to_seconds(login_expire_limit_in_minutes) 
+                
+                self.event_expire_time_in_seconds = event_expire_time_in_seconds
+                
+                self.create_account_limit = create_account_limit
+                self.create_account_time_stamp_in_days = self.days_to_seconds(create_account_time_stamp_in_days)
+                self.create_account_expire_limit_in_days = self.days_to_seconds(create_account_expire_limit_in_days)
             else:
                 raise Exception(f'os parametros fornecidos são invalidos, o cache não pode ser executado')
         except Exception as e:
@@ -37,6 +48,26 @@ class CacheHandler:
             self.r = None
     
     @staticmethod
+    def seconds_to_milliseconds(event_expire_time_in_seconds):
+        return int(event_expire_time_in_seconds*1000)
+    
+    @staticmethod
+    def minutes_to_seconds(minutes):
+        return int(minutes*60)
+    
+    @staticmethod
+    def days_to_seconds(days):
+        return int(days*60*60*24)
+    
+    @staticmethod
+    def check_if_its_a_number_parameters(*args):
+        for i in args:
+            if i is not None and isinstance(i, (int, float)) and i > 0:
+                continue
+            return False
+        return True
+    
+    @staticmethod
     def check_redis_parameters(host, port):
         if host and port:
             if isinstance(host, str) and isinstance(port, int):
@@ -44,35 +75,21 @@ class CacheHandler:
         return False
     
     @staticmethod
-    def check_initial_parameters(limit, time_stamp, expire_limit, event_expire_time):
-        if limit is not None and time_stamp is not None and expire_limit is not None and event_expire_time is not None:
-            if isinstance(limit, int) and isinstance(time_stamp, int) and isinstance(expire_limit, int) and isinstance(event_expire_time, (int, float)):
-                if limit >= 0 and time_stamp >= 0 and expire_limit >= 0 and event_expire_time >= 0:
-                    return True
-        return False
-    
-    @staticmethod
-    def validate_event_expire_time(event_expire_time):
-        return event_expire_time is not None and isinstance(event_expire_time, (int, float)) and event_expire_time > 0
-    
-    @staticmethod
-    def sanitize_event_expire_time(event_expire_time):
-        px = int(event_expire_time*1000)
-        return px 
+    def validate_event_expire_time(event_expire_time_in_seconds):
+        return event_expire_time_in_seconds is not None and isinstance(event_expire_time_in_seconds, (int, float)) and event_expire_time_in_seconds > 0
      
     @start_required
     def set_login_rate_limit(self, ip):
         try:
             key = f"black_list:{ip}"
             if not self.r.exists(key):
-                self.r.set(key, 1)
-                self.r.expire(key, self.time_stamp)
+                self.r.set(key, 1, ex=self.login_time_stamp_in_seconds)
+                attempts = 1
             else:
-                self.r.incr(key)
+                attempts = self.r.incr(key)
             
-            attempts = int(self.r.get(key))
-            if attempts > self.limit:
-                expire_time = min(self.time_stamp * (attempts - self.limit), self.expire_limit)
+            if attempts > self.login_limit:
+                expire_time = min(self.login_time_stamp_in_seconds * (attempts - self.login_limit), self.login_expire_limit_in_minutes)
                 self.r.expire(key, expire_time)
         except Exception as e:
             logging.error(f'erro ao definir o login rate, identificador do erro: {e} \n{traceback.format_exc()}')
@@ -82,14 +99,42 @@ class CacheHandler:
         try:
             key = f"black_list:{ip}"
             attempts_byte = self.r.get(key)
-            if attempts_byte is None:
+            if not attempts_byte:
                 return False
             
-            attempts = int(attempts_byte)
-            return attempts > self.limit
+            attempts = int(attempts_byte.decode('utf-8'))
+            return attempts > self.login_limit
         except Exception as e:
             logging.error(f'erro ao checar se o usuario esta no blacklist, identificador do erro: {e} \n{traceback.format_exc()}')
             return False
+    
+    @start_required
+    def create_account_limit_handler(self, ip):
+        try:
+            key = f"create_account_limit:{ip}"
+            if not self.r.exists(key):
+                self.r.set(key, 1, ex=self.create_account_time_stamp_in_days)
+                attempts = 1
+            else:
+                attempts = self.r.incr(key)
+            if attempts > self.create_account_limit:
+                expire_time = min(self.create_account_time_stamp_in_days*(attempts - self.create_account_limit), self.create_account_expire_limit_in_days)
+                self.r.expire(key, expire_time)
+        except Exception as e:
+            logging.error(f'erro ao atualizar o limite de criação de contas, identificador do erro: {e} \n{traceback.format_exc()}')
+    
+    @start_required
+    def check_create_account_validation(self, ip):
+        try:
+            key = f"create_account_limit:{ip}"
+            attempts_byte = self.r.get(key)
+            if not attempts_byte:
+                return True
+            attempts = int(attempts_byte.decode('utf-8'))
+            return attempts < self.create_account_limit
+        except Exception as e:
+            logging.error(f'erro ao checar o limite de criação de contas, identificador do erro: {e} \n{traceback.format_exc()}')
+            return True
     
     @start_required
     def ws_cache_login(self, sid, token):
@@ -106,23 +151,23 @@ class CacheHandler:
         try:
             key = f'login:{sid}'
             token_byte = self.r.get(key)
-            if token_byte is None:
+            if not token_byte:
                 return None
-            return token_byte.decode('utf-8')
+            
+            return str(token_byte.decode('utf-8'))
         except Exception as e:
             logging.error(f'erro ao buscar token de usuario: {e} \n{traceback.format_exc()}')
             return None
 
     @start_required
-    def ws_event_throttle(self, sid, event_name, event_expire_time):
+    def ws_event_throttle(self, sid, event_name, event_expire_time_in_seconds):
         try:
-            if not self.validate_event_expire_time(event_expire_time):
-                raise Exception(f'o valor de parametro fornecidos no event_expire_time é invalido, certifiquesse de passar um int ou um float')
+            if not self.validate_event_expire_time(event_expire_time_in_seconds):
+                event_expire_time_in_seconds = self.event_expire_time_in_seconds
             
-            px = self.sanitize_event_expire_time(event_expire_time)
+            px = self.seconds_to_milliseconds(event_expire_time_in_seconds)
             key = f'{event_name}:{sid}'
             if not self.r.exists(key):
-                
                 self.r.set(key, 1, px=px)
                 return True
             return False
@@ -135,7 +180,7 @@ class CacheHandler:
         try:
             key = f'login:{sid}'
             token = self.r.get(key)
-            if token is not None:
+            if token:
                 self.r.delete(key)
                 return token.decode('utf-8')
             return None            
--- route_handler.py
+++ route_handler.py
@@ -8,9 +8,20 @@ def start_route_handler(app):
     cache = CacheHandler(5, 60)
     cache.start()
     
-    def blacklist_handler(f):
+    def limit_account_handler_required(f):
         @wraps(f)
-        def wrapper(*args, **kwargs):
+        def limit_account_handler(*args, **kwargs):
+            ip = request.remote_addr
+            cache.create_account_limit_handler(ip)
+            status = cache.check_create_account_validation(ip)
+            if status:
+                return f(*args, **kwargs)
+            return jsonify({'status': False, 'msg': 'voce esta no limite de contas criadas para um usuario'}), 400
+        return limit_account_handler
+    
+    def blacklist_handler_required(f):
+        @wraps(f)
+        def blacklist_handler(*args, **kwargs):
             ip = request.remote_addr
             cache.set_login_rate_limit(ip)
             black_list_status = cache.is_black_listed(ip)
@@ -18,7 +29,7 @@ def start_route_handler(app):
                 return f(*args, **kwargs)
             else:
                 return jsonify({'status': False, 'msg': 'voce esta na blacklist para evitar bruteforce'}), 400
-        return wrapper
+        return blacklist_handler
     
     def auth_handler_required(f):
         @wraps(f)
@@ -67,7 +78,7 @@ def start_route_handler(app):
         
     @app.route('/login', methods=['POST'])
     @route_unloged_required
-    @blacklist_handler
+    @blacklist_handler_required
     @auth_handler_required
     def login(auth):
         data = request.get_json()
@@ -85,6 +96,7 @@ def start_route_handler(app):
         
     @app.route('/register', methods=["POST"])
     @route_unloged_required
+    @limit_account_handler_required
     @auth_handler_required
     def register(auth):
         data = request.get_json()
